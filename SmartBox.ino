32a4ffd4c8ddeb9e95444372410353d889d910ec
devum <doumeano0o@gmail.com>
2022-12-19 16:14:49 +0900

    시간 받아오기:NTP

--- SmartBox.ino
+++ SmartBox.ino
@@ -17,11 +17,28 @@ unsigned long lastLightTime = 0;              // 조도센서 조회 호출
 const unsigned long lightInterval = 120000L;  // 2분 마다
 
 unsigned long lastConnectionTime = 0;          // 할일 조회 호출
-const unsigned long postingInterval = 30000L;  // 60초 마다
+const unsigned long postingInterval = 30000L;  // 30초 마다
 
 WiFiEspClient client;
 String user_id = "1";
+bool all_done = false;    // 모든 할일 완료여부
 
+//---- NTP-UDP 요청 ----
+#include "WiFiEspUdp.h"
+
+unsigned long lastNTPRequest = 0;                 // 시간 조회
+const unsigned long NTPRequestInterval = 60000L;  // 60초 마다
+bool is0004 = false;                              // 00시 00분 ~ 04시 59분 인지 확인
+char timeServer[] = "time.nist.gov";  // NTP server
+unsigned int localPort = 2390;        // local port to listen for UDP packets
+
+const int NTP_PACKET_SIZE = 48;  // NTP timestamp is in the first 48 bytes of the message
+const int UDP_TIMEOUT = 2000;    // timeout in miliseconds to wait for an UDP packet to arrive
+
+byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
+WiFiEspUDP Udp;                   // UDP instance
+
+//---- Actuator -----
 #include <Servo.h>
 Servo myServo;
 bool open_flag = false;  // true : 열림, false : 닫힘
@@ -59,28 +76,45 @@ void setup() {
   Serial.println("You're connected to the network");
 
   printWifiStatus();
+  Udp.begin(localPort);
 }
 void loop() {
-  boxControl();
+  if(all_done){   // 모두 완료한 경우에만 선물박스 작동 가능
+    boxControl();
+  }
+  else{
+    myServo.write(0);
+    myServo.detach();
+  }
 
-  // LDR : lightInterval 마다 데이터 요청
-  if (millis() - lastLightTime > lightInterval) {
-    Wire.requestFrom(1, 2);
-    Serial.println();
+  // 현재시간 조회
+  if (millis() - lastNTPRequest > NTPRequestInterval) {
+    requestNTPTime();
+    lastNTPRequest = millis();
+  }
+  
+  // 00:00 ~ 04:59 AM 이면
+  if(is0004){
+    // 조도센서값 요청
+    // LDR : lightInterval 마다 데이터 요청
+    if (millis() - lastLightTime > lightInterval) {
+      Wire.requestFrom(1, 2);
+      Serial.println();
 
-    if (Wire.available() > 0) {
-      Serial.print("LDR:");
-      char update_flag = Wire.read();
-      Serial.print(update_flag);
+      if (Wire.available() > 0) {
+        Serial.print("LDR:");
+        char update_flag = Wire.read();
+        Serial.print(update_flag);
 
-      if (update_flag == '1') {
-        Serial.println("update");
-        // todo date 업데이트 요청
-        // httpRequest("/service/goals/update");
+        if (update_flag == '1') {
+          Serial.println("update");
+          // todo date 업데이트 요청
+          httpRequest("/service/goals/update");
+        }
+        lastLightTime = millis();
       }
-      lastLightTime = millis();
+      delay(1000);
     }
-    delay(1000);
   }
 
   // todo List 조회
@@ -88,8 +122,11 @@ void loop() {
     String line = client.readStringUntil('\r');
     line.replace("\n", "");
 
-    if (line.indexOf('#') != -1) {
+    if (line.indexOf('#') != -1) {  // 웹서비스 요청 Response 받기
       int isize = line.length();
+      checkAllDone(line);           // 할일 모두 완료했는지 체크
+      Serial.println("Good Data");
+
       // 32byte 이상 데이터 전송 처리 : 오래 걸림 휴
       while (isize > 0) {
         String now = "";
@@ -110,37 +147,68 @@ void loop() {
         delay(50);
       }
     }
-    delay(10);
+    else{
+      Serial.println("Bad Data");
+      Serial.println(line);
+    }
   }
   delay(100);
 
   // postingInterval 마다 데이터 조회
   if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest("/service/goals/" + user_id);
+    // httpRequest("/service/goals/" + user_id);
+    httpRequest("/service/goals/connected");
   }
+}
+
+// 할일 모두 완료했는지 체크
+void checkAllDone(String res){
+  // Response 문자열 형식 : led색상10진수|총갯수,완료갯수|메세지,positive메세지갯수
+  if(res.indexOf('|') == -1) return;
+  // {led색상10진수} 제거
+  res = res.substring(res.indexOf('|') + 1);
+  if(res.indexOf('|') == -1) return;
+  // {메세지,positive메세지갯수} 제거
+  res = res.substring(0, res.indexOf('|'));
+  if(res.indexOf(',') == -1) return;
+
+  //{갯수,완료갯수}
+  int comma_idx = res.indexOf(',');
+  int total_cnt = res.substring(0, comma_idx).toInt();
+  int done_cnt = res.substring(comma_idx + 1).toInt();
+
+  if(total_cnt > 0 && total_cnt == done_cnt)   // 등록된 할일이 있고, 모두 완료한 경우
+  {
+    all_done = true;
+  }
+  else{
+    all_done = false;
+  }
+  Serial.print("all done:");
+  Serial.println(all_done);
 }
 
 // 상자 열기
 void boxControl() { 
-  // 초음파 센서 값 받아오기 
+  // 초음파 센서 측정
   digitalWrite(TRIG, LOW);
+  digitalWrite(ECHO, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);   // 초음파 보내기
   delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
+  digitalWrite(TRIG, LOW);  
   
-  // 초음파 반사되어 돌려받은 시간 저장
-  duration = pulseIn(ECHO, HIGH); 
-  // 거리 계산 (cm 변환)
-  distance = duration * 17.0 / 1000.0;
+  duration = pulseIn(ECHO, HIGH);       // 초음파 반사되어 돌려받은 시간 저장
+  distance = duration * 17.0 / 1000.0;  // 거리 계산 (cm 변환)
 
-  Serial.print("distance");
-  Serial.println(distance);
-  delay(100);
+  delay(1000);
   int open = 0;
-  int close = 120;
+  int close = 100;
 
-  if (open_flag && distance < 22) {   // 닫혀있고, 앞에 뭐가 있는 경우에만
+  if (!open_flag && distance < 20) {   // 닫혀있고, 앞에 뭐가 있는 경우에만
+    Serial.print("distance : ");
+    Serial.print(distance);
+    Serial.println(" (open) ");
     myServo.attach(4);
     open_flag = !open_flag;
     // 90: open,  180: close
@@ -150,10 +218,13 @@ void boxControl() {
     }
     myServo.detach();
     // 징글벨 재생
-    // jinglebell();
+    jinglebell();
     delay(100);
   }
-  else if(!open_flag && distance > 22){    // 열려있는데, 앞에 뭐가 없으면
+  else if(open_flag && distance > 50){    // 열려있는데, 앞에 뭐가 없으면 닫기
+    Serial.print("distance : ");
+    Serial.print(distance);
+    Serial.println(" (close) ");
     myServo.attach(4);
     open_flag = !open_flag;
     for (int pos = open; pos < close; pos++) {
@@ -163,21 +234,7 @@ void boxControl() {
     myServo.detach();
 
     // 울면안돼 재생
-    // noCry();
+    noCry();
     delay(100);
   }
 }
-
-// 상자 닫기
-void close() {
-  for (int pos = 90; pos >= 0; pos--) {
-    myServo.write(pos);
-    delay(15);
-  }
-  myServo.write(0);
-
-  // 경고음 재생
-  ring(2, 330);
-}
-
-//---- NTP ----
---
+++ UTPNTP_Time.ino
@@ -0,0 +1,81 @@
+// NTP UDP 패킷 전송
+void sendNTPpacket(char *ntpSrv)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(ntpSrv, 123); //NTP requests are to port 123
+
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+
+  Udp.endPacket();
+}
+
+// NTP 시간 조회
+void requestNTPTime(){
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+  
+  // wait for a reply for UDP_TIMEOUT miliseconds
+  unsigned long startMs = millis();
+  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
+
+  Serial.println(Udp.parsePacket());
+  if (Udp.parsePacket()) {
+    Serial.println("packet received");
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);    // 받은 패킷 버퍼로 읽어들이기
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.println(secsSince1900);
+
+    // 1900.01.01 부터 계산
+    const unsigned long seventyYears = 2208988800UL;
+    unsigned long epoch = secsSince1900 - seventyYears;
+    epoch += 32400;   // 한국시간 + 32400
+
+    //- 시
+    int hour = (epoch  % 86400L) / 3600;
+    if(hour >= 0 && hour <= 4)
+    {
+      is0004 = true;    // 새벽 00:00~04:59 여부 업데이트
+      Serial.print(hour);
+      Serial.println(" AM");
+    }else{
+      is0004 = false;
+    }
+    // HH:mm:ss
+    Serial.print("Time : ");       // UTC is the time at Greenwich Meridian (GMT)
+    
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if (((epoch % 3600) / 60) < 10) { // 0 패딩
+      Serial.print('0');  
+    }
+    //- 분
+    int minute = (epoch  % 3600) / 60;
+    Serial.print(minute); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if (minute < 10) {  // 0 패딩
+      Serial.print('0');
+    }
+    //- 초
+    Serial.println(epoch % 60);
+  }
+
+  delay(10);
+}
